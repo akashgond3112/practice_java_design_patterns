f489ef33e196316af2df759a33f2661e20e182f5
akashgond3112 <akashgond3112@gmail.com>
2025-01-18 17:01:03 +0000

    Commit : 18/01/2025

--- CircuitBreakerPattern/src/main/java/com/java/design/patterns/PaymentProcessor.java
+++ CircuitBreakerPattern/src/main/java/com/java/design/patterns/PaymentProcessor.java
@@ -1,7 +1,10 @@
 package com.java.design.patterns;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Random;
 
+@Slf4j
 public class PaymentProcessor implements PaymentGatewayService {
 	private final long processingTime;
 	private final double failureRate;
@@ -14,14 +17,15 @@ public class PaymentProcessor implements PaymentGatewayService {
 
 	@Override
 	public String processPayment(double amount) throws Exception {
-		// Simulate processing delay
+		log.info("Processing payment of ${}", amount);
 		Thread.sleep(processingTime);
 
-		// Simulate random failures
 		if (random.nextDouble() < failureRate) {
+			log.error("Payment processing failed for amount ${}", amount);
 			throw new Exception("Payment processing failed");
 		}
 
+		log.info("Payment processed successfully for amount ${}", amount);
 		return "Payment processed successfully: $" + amount;
 	}
 }
