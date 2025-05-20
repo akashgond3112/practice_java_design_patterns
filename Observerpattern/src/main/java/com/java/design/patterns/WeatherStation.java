84af7577e16be2d94a1f0df6152c0e41996aca9b
akashgond3112 <akashgond3112@gmail.com>
2025-05-20 07:49:39 +0100

    Commit : 20/05/2025

--- Observerpattern/src/main/java/com/java/design/patterns/WeatherStation.java
+++ Observerpattern/src/main/java/com/java/design/patterns/WeatherStation.java
@@ -1,4 +1,70 @@
 package com.java.design.patterns;
 
-public class WeatherStation {
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The WeatherStation class represents the Subject in the Observer pattern. It maintains a list of observers and
+ * notifies them of any state changes.
+ */
+public class WeatherStation implements Subject {
+	private final List<Observer> observers;
+	// Getters for the weather measurements
+	@Getter
+	private float temperature;
+	@Getter
+	private float humidity;
+	@Getter
+	private float pressure;
+
+	public WeatherStation() {
+		observers = new ArrayList<>();
+	}
+
+	@Override
+	public void registerObserver(Observer o) {
+		observers.add(o);
+	}
+
+	@Override
+	public void removeObserver(Observer o) {
+		int i = observers.indexOf(o);
+		if (i >= 0) {
+			observers.remove(i);
+		}
+	}
+
+	@Override
+	public void notifyObservers() {
+		for (Observer observer : observers) {
+			observer.update(temperature, humidity, pressure);
+		}
+	}
+
+	/**
+	 * Called when weather measurements have been updated
+	 */
+	public void measurementsChanged() {
+		notifyObservers();
+	}
+
+	/**
+	 * Sets the weather measurements and triggers notification
+	 *
+	 * @param temperature
+	 * 		the current temperature
+	 * @param humidity
+	 * 		the current humidity
+	 * @param pressure
+	 * 		the current pressure
+	 */
+	public void setMeasurements(float temperature, float humidity, float pressure) {
+		this.temperature = temperature;
+		this.humidity = humidity;
+		this.pressure = pressure;
+		measurementsChanged();
+	}
+
 }
