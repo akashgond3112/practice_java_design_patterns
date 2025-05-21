89965f322ae9db824ec0e40974d3f55ca9ae63e6
akashgond3112 <akashgond3112@gmail.com>
2025-05-21 15:05:40 +0100

    Commit : 21/05/2025

--- Observerpattern/src/main/java/com/java/design/patterns/CurrentConditionsDisplay.java
+++ Observerpattern/src/main/java/com/java/design/patterns/CurrentConditionsDisplay.java
@@ -1,4 +1,33 @@
 package com.java.design.patterns;
 
-public class CurrentConditionsDisplay {
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CurrentConditionsDisplay implements Observer, DisplayElement {
+	// Used for testing
+	@Getter
+	private float temperature;
+	@Getter
+	private float humidity;
+	@Getter
+	private final Subject weatherData;
+
+	public CurrentConditionsDisplay(Subject weatherData) {
+		this.weatherData = weatherData;
+		weatherData.registerObserver(this);
+	}
+
+	@Override
+	public void update(float temperature, float humidity, float pressure) {
+		this.temperature = temperature;
+		this.humidity = humidity;
+		display();
+	}
+
+	@Override
+	public void display() {
+		log.info(String.format("Current conditions: %.1f°C and %.1f%% humidity%n", temperature, humidity));
+	}
+
 }
