3e1c7dbf31272e11737a571cd8a304e2773ad9a7
akashgond3112 <akashgond3112@gmail.com>
2025-05-24 20:12:34 +0100

    Commit : 23/05/2025

--- Observerpattern/src/main/java/com/java/design/patterns/ForecastDisplay.java
+++ Observerpattern/src/main/java/com/java/design/patterns/ForecastDisplay.java
@@ -1,4 +1,34 @@
 package com.java.design.patterns;
 
-public class ForecastDisplay {
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ForecastDisplay implements Observer, DisplayElement {
+	// Used for testing
+	@Getter
+	private float currentPressure = 29.92f;
+	@Getter
+	private float lastPressure;
+	private final Subject weatherData;
+
+	public ForecastDisplay(Subject weatherData) {
+		this.weatherData = weatherData;
+		weatherData.registerObserver(this);
+	}
+
+	@Override
+	public void update(float temp, float humidity, float pressure) {
+		lastPressure = currentPressure;
+		currentPressure = pressure;
+		display();
+	}
+
+	@Override
+	public void display() {
+		log.info("Forecast: {}", currentPressure > lastPressure ?
+				"Improving weather on the way!" :
+				currentPressure == lastPressure ? "More of the same" : "Watch out for cooler, rainy weather");
+	}
+
 }
