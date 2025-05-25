f143d9e384224e5f321e1e5846dc98d536057d0e
akashgond3112 <akashgond3112@gmail.com>
2025-05-25 19:26:22 +0100

    Commit : 24/05/2025

--- Observerpattern/src/main/java/com/java/design/patterns/WeatherStationDemo.java
+++ Observerpattern/src/main/java/com/java/design/patterns/WeatherStationDemo.java
@@ -1,4 +1,35 @@
 package com.java.design.patterns;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class WeatherStationDemo {
+	public static void main(String[] args) {
+		// Create the WeatherStation (Subject)
+		WeatherStation weatherStation = new WeatherStation();
+
+		// Create and register the displays (Observers)
+		CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherStation);
+		StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherStation);
+		ForecastDisplay forecastDisplay = new ForecastDisplay(weatherStation);
+
+		log.info("Weather station initialized with three observers.");
+		log.info("Simulating weather changes...\n");
+
+		// Simulate weather changes
+		weatherStation.setMeasurements(26.0f, 65.0f, 30.4f);
+		log.info("");
+
+		weatherStation.setMeasurements(28.0f, 70.0f, 29.2f);
+		log.info("");
+
+		weatherStation.setMeasurements(22.0f, 90.0f, 29.2f);
+		log.info("");
+
+		// Remove an observer
+		log.info("Removing current conditions display...");
+		weatherStation.removeObserver(currentDisplay);
+
+		weatherStation.setMeasurements(24.0f, 80.0f, 30.0f);
+	}
 }
