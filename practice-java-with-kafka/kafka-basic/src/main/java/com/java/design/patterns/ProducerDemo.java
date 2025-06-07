9d7d08e2216374cbaa7f612251ae7bce4ef13f54
akashgond3112 <akashgond3112@gmail.com>
2025-06-07 19:31:59 +0100

    Add Kafka setup with Docker and implement ProducerDemo class

--- practice-java-with-kafka/kafka-basic/src/main/java/com/java/design/patterns/ProducerDemo.java
+++ practice-java-with-kafka/kafka-basic/src/main/java/com/java/design/patterns/ProducerDemo.java
@@ -1,4 +1,23 @@
 package com.java.design.patterns;
 
+import java.util.Properties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ProducerDemo {
+
+	private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class);
+
+	public static void main(String[] args) {
+		log.info("Starting producer demo");
+
+		Properties properties = new Properties();
+
+		// Connect to local Kafka setup
+		properties.setProperty("bootstrap.servers", "localhost:9092");
+		properties.setProperty("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		properties.setProperty("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+		log.info("Producer properties set for local Kafka setup");
+	}
 }
