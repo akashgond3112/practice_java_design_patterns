04a8fc0c166e8d562945133dc5136b137bca3ad6
akashgond3112 <akashgond3112@gmail.com>
2025-06-08 09:48:41 +0100

    Commit : 08/06/2025

---
+++ practice-java-with-kafka/kafka-basic/src/main/java/com/java/design/patterns/ConsumerDemoWithCooperative.java
@@ -0,0 +1,86 @@
+package com.java.design.patterns;
+
+import org.apache.kafka.clients.consumer.ConsumerPartitionAssignor;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.CooperativeStickyAssignor;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Properties;
+
+public class ConsumerDemoWithCooperative {
+
+	private static final Logger log = LoggerFactory.getLogger(ConsumerDemoWithCooperative.class);
+
+	public static void main(String[] args) {
+		log.info("Starting Consumer demo with cooperative");
+
+		// Load the logging configuration file
+		System.setProperty("org.slf4j.simpleLogger.properties", "src/main/resources/simplelogger.properties");
+
+		String groupId = "my-group";
+
+		Properties properties = new Properties();
+
+		// Connect to local Kafka setup
+		properties.setProperty("bootstrap.servers", "localhost:9092");
+
+		// create consumer configs
+		properties.setProperty("key.deserializer", StringDeserializer.class.getName());
+		properties.setProperty("value.deserializer", StringDeserializer.class.getName());
+
+		properties.setProperty("group.id", groupId);
+		properties.setProperty("auto.offset.reset", "earliest");
+
+		properties.setProperty("partition.assignment.strategy", CooperativeStickyAssignor.class.getName());
+
+		KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+		// get reference to the current thread
+		final Thread mainThread = Thread.currentThread();
+
+		// add the shut-down hook
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
+			public void run() {
+				log.info("Shutting down");
+				consumer.wakeup();
+
+				// join the main thread to allow the execution of the code
+				try {
+					mainThread.join();
+				} catch (InterruptedException e) {
+					throw new RuntimeException(e);
+				}
+
+			}
+		});
+		//poll
+		try {
+			consumer.subscribe(List.of("demo_java"));
+
+			while (true) {
+				log.info("Polling");
+				ConsumerRecords<String, String> records = consumer.poll(java.time.Duration.ofMillis(1000));
+				for (ConsumerRecord<String, String> record : records) {
+					log.info("key {}", record.key());
+					log.info("value {}", record.value());
+					log.info("partition {}", record.partition());
+					log.info("offset {}", record.offset());
+				}
+			}
+		} catch (WakeupException e) {
+			log.info("Consumer is starting to Shutting down");
+		} catch (Exception e) {
+			log.error("Error", e);
+		} finally {
+			log.info("Shutting down gracefully");
+			consumer.close();
+		}
+	}
+}
