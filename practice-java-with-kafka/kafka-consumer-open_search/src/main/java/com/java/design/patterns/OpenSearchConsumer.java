f09de435d22d741f56fd27e89091ce3dec4c3ceb
akashgond3112 <akashgond3112@gmail.com>
2025-06-11 22:16:06 +0100

    Add OpenSearch consumer implementation and update dependencies in pom.xml

---
+++ practice-java-with-kafka/kafka-consumer-open_search/src/main/java/com/java/design/patterns/OpenSearchConsumer.java
@@ -0,0 +1,11 @@
+package com.java.design.patterns;
+
+public class OpenSearchConsumer {
+
+	public static void main(String[] args) {
+		// Create a new OpenSearchConsumer instance
+		OpenSearchConsumer consumer = new OpenSearchConsumer();
+
+
+	}
+}
