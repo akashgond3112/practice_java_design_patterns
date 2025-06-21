d9d5d9b3b0547ee82d49ff9cf657ed1fbb34bfae
akashgond3112 <akashgond3112@gmail.com>
2025-06-21 09:40:29 +0100

    Add unit test for MySourceConnectorConfig

---
+++ practice-java-with-kafka/kafka-connect-arch-type/src/test/java/com/java/design/patterns/MySourceConnectorConfigTest.java
@@ -0,0 +1,10 @@
+package com.java.design.patterns;
+
+import org.junit.Test;
+
+public class MySourceConnectorConfigTest {
+  @Test
+  public void doc() {
+    System.out.println(MySourceConnectorConfig.conf().toRst());
+  }
+}
